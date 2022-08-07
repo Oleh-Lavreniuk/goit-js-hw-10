65969ad6417567c8ddd5f140935405db4b80a289
Oleh-Lavreniuk <oleh4380211@gmail.com>
2022-08-08 01:58:44 +0300

    add fetch request and response filtration

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 import './css/styles.css';
 
+import { fetchCountries } from './js/fetchCountries';
+
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import debounce from 'lodash.debounce';
+
 const DEBOUNCE_DELAY = 300;
+
+const inputCountry = document.querySelector('#search-box');
+const countryList = document.querySelector('.country-list');
+const countryInfo = document.querySelector('#search-box');
+
+inputCountry.addEventListener(
+  'input',
+  debounce(onSearchCountries, DEBOUNCE_DELAY)
+);
+
+function onSearchCountries(ev) {
+  fetchCountries(ev.target.value)
+    .then(data => {
+      console.log(data);
+    })
+    .catch(error => {
+      console.log(error);
+    });
+}
---
+++ src/js/fetchCountries.js
@@ -0,0 +1,14 @@
+const BASE_URL = 'https://restcountries.com/v3.1';
+const FILTER_RESPONSE = 'name,capital,population,flags,languages';
+
+export function fetchCountries(name) {
+  return fetch(`${BASE_URL}/name/{name}?fields=${FILTER_RESPONSE}`).then(
+    response => {
+      if (!response.ok) {
+        throw new Error(response.status);
+      }
+
+      return response.json();
+    }
+  );
+}
